8c4576b9ea214d553c5c1d9dfe46fee2dcdfb314
kurczinho <kurczinho@kurczinho.com>
2015-05-25 16:57:57 +0200

    modularization

--- db_gen_faker.js
+++
@@ -1,9 +0,0 @@
-module.exports = function() {
-  var data = { users: [] }
-  var faker = require("faker");
-  // Create 1000 users 
-  for (var i = 0; i < 8; i++) {
-    data.users.push({ id: i, name: faker.name.findName() })
-  }
-  return data
-}
